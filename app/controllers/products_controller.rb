27750220243e32d7f8f3bc5eb0435009a43a5cec
Ada <larmartinez2@gmail.com>
2019-01-09 15:09:41 -0800

    show, create, and index actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,31 @@
 class ProductsController < ApplicationController
   def show
+    product = Product.find_by(id: params[:id])
+
+    if product
+      render json: product.as_json(only: [:id, :upc, :name, :image, :date, :notes, :description, :color, :brand]), status: :ok
+    else
+      render json: {ok: false, message: 'product not found'}, status: :not_found
+    end
   end
 
   def index
+    products = Product.all
+    if products.nil?
+      render json: {ok:false, message: 'not_found'}, status: :not_found
+    else
+      render json: products.as_json(only: [:id, :upc, :name, :image, :date, :notes, :description, :color, :brand]), status: :ok
+    end
   end
 
   def create
+    product = Product.new(product_params)
+
+   if product.save
+     render json: product.as_json(only: [:id]), status: :ok
+   else
+     render json: {ok: false, message: product.errors.messages}, status: :bad_request
+   end
   end
 
   def new
@@ -25,5 +45,11 @@ class ProductsController < ApplicationController
 
   def zomg
     render json: {message: "it works!"}, status: :ok
+  end
+
+  private
+  
+  def product_params
+     params.permit(:name, :image, :date, :notes, :description, :brand, :color, :upc)
   end
 end
