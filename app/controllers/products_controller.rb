09d5f49894fef4068065b0141dffafe8aa75c197
Ada <larmartinez2@gmail.com>
2019-01-09 12:29:54 -0800

    create Products controller

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,25 @@
+class ProductsController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def overdue
+  end
+end
