9827def7c14ede39f9747186bd0bd00dec230535
Ada <larmartinez2@gmail.com>
2019-01-09 12:42:46 -0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'products/show'
-  get 'products/index'
-  get 'products/create'
-  get 'products/new'
-  get 'products/edit'
-  get 'products/update'
-  get 'products/destroy'
-  get 'products/overdue'
+  resources :products
+  get "/products/overdue", to: "products#overdue", as: "overdue"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
