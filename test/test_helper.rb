ed4aebc3af0b3afba6bd3fabc8e3d0c07d80f80e
Ada <larmartinez2@gmail.com>
2019-01-10 15:25:39 -0800

    show which tests passed

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
+Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new()]
 
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
