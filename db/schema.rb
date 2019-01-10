d706d6c71b1db9a9fc5de9e688250aa5060f150f
Ada <larmartinez2@gmail.com>
2019-01-10 11:01:55 -0800

    add the pao column to the model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_09_200444) do
+ActiveRecord::Schema.define(version: 2019_01_10_185610) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_01_09_200444) do
     t.string "upc"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "pao"
   end
 
 end
