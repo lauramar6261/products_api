2badd61863dadf5fd61bf649edbb0cf523772eef
Ada <larmartinez2@gmail.com>
2019-01-12 15:40:19 -0800

    create user model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   validates :name, :upc, :image, presence: true
-
+  belongs_to :user
   def self.overdue
     self.where("date < ?", Date.today)
-  end  
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_10_185610) do
+ActiveRecord::Schema.define(version: 2019_01_12_232252) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2019_01_10_185610) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "pao"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
