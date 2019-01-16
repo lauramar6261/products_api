a1b880f5d5471570c893c55baf3ba7026bf6d6ed
Ada <larmartinez2@gmail.com>
2019-01-15 17:23:32 -0800

    upc must be unique

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   validates :name, :upc, :image, presence: true
+  validates :upc, uniqueness: true
   def self.overdue
     self.where("date < ?", Date.today)
   end
