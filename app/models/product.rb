f9770e6da7d924a494601cbfbf12430829425195
Ada <larmartinez2@gmail.com>
2019-01-18 00:39:30 -0800

    error must in in the message key

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,13 +2,13 @@ class Product < ApplicationRecord
   validates :name, :upc, :image, presence: true
   validates :upc, uniqueness: true
 
-  validate :expiration_date_cannot_be_in_the_past,
+  validate :expiration_date_cannot_be_in_the_past
   def self.overdue
     self.where("date < ?", Date.today)
   end
 
   def expiration_date_cannot_be_in_the_past
-    errors.add("expiration date must be in the future") if
+    errors.message.add("expiration date must be in the future") if
       !date.blank? and date <= Date.today
   end
 end
